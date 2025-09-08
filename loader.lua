d8d11ff67e60e8fe267fbd915130ef80385cbb3c
trieuvancoi <ccoilopc@gmail.com>
2025-09-08 12:49:50 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,20 +1,12 @@
-local player = game.Players.LocalPlayer
+-- Redz Kill Aura | Loader
+local url = "https://raw.githubusercontent.com/trieuvancoi/Redz-kill/main/Main.lua"
 
-local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.ResetOnSpawn = false
-ScreenGui.IgnoreGuiInset = true
-ScreenGui.Name = "RedzTest"
-ScreenGui.Parent = player:WaitForChild("PlayerGui")
+local success, response = pcall(function()
+    return game:HttpGet(url)
+end)
 
-local frame = Instance.new("Frame")
-frame.Size = UDim2.new(0, 200, 0, 100)
-frame.Position = UDim2.new(0.5, -100, 0.5, -50)
-frame.BackgroundColor3 = Color3.fromRGB(255,0,0)
-frame.Parent = ScreenGui
-
-local label = Instance.new("TextLabel")
-label.Size = UDim2.new(1,0,1,0)
-label.Text = "Redz Kill Aura Menu"
-label.TextColor3 = Color3.new(1,1,1)
-label.BackgroundTransparency = 1
-label.Parent = frame
+if success then
+    loadstring(response)()
+else
+    warn("[Redz Kill Aura] Không thể tải script từ GitHub")
+end
