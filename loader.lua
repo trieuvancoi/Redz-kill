577e4be6d1fe47cf0cc765403bd7d04dbe52728c
trieuvancoi <ccoilopc@gmail.com>
2025-09-08 12:10:55 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,18 @@
+-- Redz Kill Aura | Loader
 local url = "https://raw.githubusercontent.com/trieuvancoi/Redz-kill/main/Main.lua"
+
 local success, response = pcall(function()
     return game:HttpGet(url)
 end)
-if success then
-    loadstring(response)()
+
+if success and response then
+    local func, err = loadstring(response)
+    if func then
+        func()
+        print("[Redz Kill Aura] Main.lua loaded thành công!")
+    else
+        warn("[Redz Kill Aura] Lỗi loadstring:", err)
+    end
 else
     warn("[Redz Kill Aura] Không thể tải script từ GitHub")
 end
