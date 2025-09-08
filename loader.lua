bee9252c50c5af4bfc3d5fd97bc9ffc2336dc208
trieuvancoi <ccoilopc@gmail.com>
2025-09-08 12:57:24 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 -- Redz Kill Aura | Loader
-local url = "https://raw.githubusercontent.com/trieuvancoi/Redz-kill/main/Main.lua"
+local url = "https://raw.githubusercontent.com/<username>/<repo>/main/Main.lua"
 
 local success, response = pcall(function()
     return game:HttpGet(url)
