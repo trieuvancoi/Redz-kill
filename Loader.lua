8aae4f38a846b5f96925bd1bcedf6b764354193c
trieuvancoi <ccoilopc@gmail.com>
2025-09-08 10:49:15 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,12 @@
+-- Redz Kill Aura | Loader
+local url = "https://raw.githubusercontent.com/<USERNAME>/<REPO>/main/Main.lua"
+
+local success, response = pcall(function()
+    return game:HttpGet(url)
+end)
+
+if success then
+    loadstring(response)()
+else
+    warn("[Redz Kill Aura] Không thể tải script từ GitHub!")
+end
